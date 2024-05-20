61f061c136d9c23f9c2b47c75833d1b6ba5b7a31
lee kinanga <lee254kinanga@gmail.com>
2024-05-20 10:51:08 +0300

    Home page responsive to all devices

--- js/index.js
+++ js/index.js
@@ -21,9 +21,38 @@ document.getElementById("open").addEventListener("click", () => {
     Showsidebar();
 });
 
-document.getElementById("sidenav").addEventListener('click', () => {
-    Hidesidebar();
-});
+// document.getElementById("sidenav").addEventListener('click', () => {
+//     Hidesidebar();
+// });
 
 // Add scroll event listener to hide the side navigation bar on scroll
-window.addEventListener("scroll", handleScroll);
+// window.addEventListener("scroll", handleScroll);
+
+
+const homeElement = document.querySelector('.home');
+const images = [ // Replace with your image URLs
+  "url(../images/bgs/man-looking.jpg)",
+  "url(../images/bgs/bulldozer.jpg)",
+  "url(../images/bgs/town city.jpg)",
+  "url(../images/bgs/unfinished tower.jpg)",
+];
+
+let currentImageIndex = 0;
+
+function changeImage() {
+  homeElement.classList.add('fade-out'); // Add a class for fading out
+
+  setTimeout(() => {
+    homeElement.style.backgroundImage = images[currentImageIndex];
+    homeElement.classList.remove('fade-out'); // Remove fade-out class
+    homeElement.classList.add('fade-in'); // Add a class for fading in
+  }, 1000); // Delay after fade-out for transition effect (adjust as needed)
+
+  currentImageIndex = (currentImageIndex + 1) % images.length;
+}
+
+setInterval(changeImage, 5000); // Change image every 5 seconds
+
+// Initial image display
+changeImage();
+
