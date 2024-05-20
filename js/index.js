50d78a4cc45b2898c677e3f1b6199dccdedea052
lee kinanga <lee254kinanga@gmail.com>
2024-05-20 16:51:12 +0300

    bg edit

--- js/index.js
+++ js/index.js
@@ -29,30 +29,30 @@ document.getElementById("open").addEventListener("click", () => {
 // window.addEventListener("scroll", handleScroll);
 
 
-const homeElement = document.querySelector('.home');
-const images = [ // Replace with your image URLs
-  "url(../images/bgs/man-looking.jpg)",
-  "url(../images/bgs/bulldozer.jpg)",
-  "url(../images/bgs/town city.jpg)",
-  "url(../images/bgs/unfinished tower.jpg)",
-];
+// const homeElement = document.querySelector('.home');
+// const images = [ // Replace with your image URLs
+//   "url(../images/bgs/man-looking.jpg)",
+//   "url(../images/bgs/bulldozer.jpg)",
+//   "url(../images/bgs/town city.jpg)",
+//   "url(../images/bgs/unfinished tower.jpg)",
+// ];
 
-let currentImageIndex = 0;
+// let currentImageIndex = 0;
 
-function changeImage() {
-  homeElement.classList.add('fade-out'); // Add a class for fading out
+// function changeImage() {
+//   homeElement.classList.add('fade-out'); // Add a class for fading out
 
-  setTimeout(() => {
-    homeElement.style.backgroundImage = images[currentImageIndex];
-    homeElement.classList.remove('fade-out'); // Remove fade-out class
-    homeElement.classList.add('fade-in'); // Add a class for fading in
-  }, 1000); // Delay after fade-out for transition effect (adjust as needed)
+//   setTimeout(() => {
+//     homeElement.style.backgroundImage = images[currentImageIndex];
+//     homeElement.classList.remove('fade-out'); // Remove fade-out class
+//     homeElement.classList.add('fade-in'); // Add a class for fading in
+//   }, 1000); // Delay after fade-out for transition effect (adjust as needed)
 
-  currentImageIndex = (currentImageIndex + 1) % images.length;
-}
+//   currentImageIndex = (currentImageIndex + 1) % images.length;
+// }
 
-setInterval(changeImage, 5000); // Change image every 5 seconds
+// setInterval(changeImage, 5000); // Change image every 5 seconds
 
-// Initial image display
-changeImage();
+// // Initial image display
+// changeImage();
 
